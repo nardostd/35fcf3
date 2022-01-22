bc47c11856b6ef7ce47dfa00b326031c4e32c407
nardostd <nardostd@gmail.com>
2022-01-22 07:24:18 -0600

    The ProspectFile schema is modified. Optional fields like first_name_index and has_header are changed to nullable/optional.

--- server/api/schemas/prospect_file.py
+++ server/api/schemas/prospect_file.py
@@ -10,10 +10,10 @@ class ProspectFile(BaseModel):
     sha512_digest: str
     file_path: str
     email_index: int
-    first_name_index: int
-    last_name_index: int
-    has_header: bool
-    force: bool
+    first_name_index: Optional[int]
+    last_name_index: Optional[int]
+    has_header: Optional[bool]
+    force: Optional[bool]
     rows_total: int
     rows_done: int
     uploaded_at: datetime
